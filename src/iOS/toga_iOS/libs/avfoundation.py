0a1473a79d95bbd5ba5ee6d9afc67315158f9c88
Isaiah Hewitt <isaiah@hewitts.us>
2020-08-06 10:20:28 -0700

    Added CommandCenter

--- src/iOS/toga_iOS/libs/avfoundation.py
+++ src/iOS/toga_iOS/libs/avfoundation.py
@@ -8,6 +8,7 @@ from rubicon.objc import *
 
 ######################################################################
 avfoundation = cdll.LoadLibrary(util.find_library('AVFoundation'))
+media_player = cdll.LoadLibrary(util.find_library('MediaPlayer'))
 ######################################################################
 
 ######################################################################
@@ -15,3 +16,4 @@ avfoundation = cdll.LoadLibrary(util.find_library('AVFoundation'))
 AVMIDIPlayer = ObjCClass('AVMIDIPlayer')
 AVAudioSession = ObjCClass('AVAudioSession')
 AVAudioSessionCategoryPlayback = 'AVAudioSessionCategoryPlayback'
+CommandCenter = ObjCClass('MPRemoteCommandCenter')
